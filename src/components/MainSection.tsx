e2bb9bf96b2a2eb23e68ac1b27ad0f8949e1bf83
s4nj1th <sanjith@tutamail.com>
2025-07-15 18:03:33 +0530

    fixed theatre mode

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -23,7 +23,6 @@ export default function MainSection() {
   const [theatreMode, setTheatreMode] = useState(false);
   const [copySuccess, setCopySuccess] = useState("");
   const [showToast, setShowToast] = useState(false);
-  const playerRef = useRef<HTMLDivElement>(null);
 
   const handleSeasonChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -107,21 +106,6 @@ export default function MainSection() {
   }, [theatreMode]);
 
   useEffect(() => {
-    if (!theatreMode) return;
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        playerRef.current &&
-        !playerRef.current.contains(event.target as Node) &&
-        embedUrl != null
-      ) {
-        setTheatreMode(false);
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, [theatreMode]);
-
-  useEffect(() => {
     if (embedUrl) {
       saveToHistory({
         url,
@@ -144,168 +128,175 @@ export default function MainSection() {
       if ((e.key === "t" || e.key === "T") && embedUrl != null) {
         setTheatreMode((prev) => !prev);
       }
+      if (e.key === "Escape") {
+        setTheatreMode(false);
+      }
     };
 
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []);
+  }, [embedUrl]);
 
   return (
-    <div className="flex flex-col-reverse md:flex-row items-start justify-center gap-10 p-6 min-h-screen text-white w-full relative z-10 md:mt-20 -mt-20">
-      <div className="w-full max-w-[300px] md:mx-0 mx-auto md:my-4 space-y-4">
-        <div className="flex gap-4 justify-center md:justify-end">
-          {(["movie", "series"] as const).map((type) => (
-            <button
-              key={type}
-              onClick={() => setMediaType(type)}
-              className={`px-4 py-2 rounded border transition-all duration-300 cursor-pointer ${
-                mediaType === type
-                  ? "bg-white text-black font-black border-white"
-                  : "bg-transparent text-white border-[#111] hover:bg-[#222e]"
-              }`}
-            >
-              {type.charAt(0).toUpperCase() + type.slice(1)}
-            </button>
-          ))}
-        </div>
-
-        <div className="relative w-full max-w-lg">
-          <input
-            type="url"
-            placeholder="Enter IMDb or TMDB URL"
-            value={url}
-            onChange={handleUrlChange}
-            className="w-full bg-[#111] text-white border border-[#181818] rounded px-4 py-2 pr-10 outline-none"
-          />
-          {url ? (
-            <button
-              onClick={() => setUrl("")}
-              className="absolute right-3 top-1/2 -translate-y-1/2 text-white hover:text-gray-300"
-            >
-              <MdClose className="text-lg" />
-            </button>
-          ) : (
-            <button
-              onClick={async () => {
-                try {
-                  const text = await navigator.clipboard.readText();
-                  if (text) setUrl(text);
-                } catch (err) {
-                  console.error("Clipboard read failed:", err);
-                }
-              }}
-              className="absolute right-3 top-1/2 -translate-y-1/2 text-white hover:text-gray-300"
-            >
-              <IoMdClipboard className="text-lg" />
-            </button>
-          )}
-        </div>
-
-        {mediaType === "series" && (
-          <div className="space-y-2 w-full max-w-lg">
-            {[
-              {
-                label: "Season",
-                value: season,
-                onChange: handleSeasonChange,
-              },
-              {
-                label: "Episode",
-                value: episode,
-                onChange: handleEpisodeChange,
-              },
-            ].map(({ label, value, onChange }) => (
-              <div className="flex w-full" key={label}>
-                <span className="w-24 text-center px-4 py-2 bg-[#111] border border-r-0 border-[#111] rounded-l text-sm">
-                  {label}
-                </span>
-                <input
-                  type="number"
-                  min={1}
-                  value={value}
-                  onChange={onChange}
-                  placeholder="1"
-                  className="flex-1 w-full bg-[#0d0d0d] text-white border border-l-0 border-[#111] rounded-r px-4 py-2 outline-none"
-                />
-              </div>
+    <>
+      <div className="flex flex-col-reverse md:flex-row items-start justify-center gap-10 p-6 min-h-screen text-white w-full relative z-10 md:mt-20 -mt-20">
+        <div className="w-full max-w-[300px] md:mx-0 mx-auto md:my-4 space-y-4">
+          <div className="flex gap-4 justify-center md:justify-end">
+            {(["movie", "series"] as const).map((type) => (
+              <button
+                key={type}
+                onClick={() => setMediaType(type)}
+                className={`px-4 py-2 rounded border transition-all duration-300 cursor-pointer ${
+                  mediaType === type
+                    ? "bg-white text-black font-black border-white"
+                    : "bg-transparent text-white border-[#111] hover:bg-[#222e]"
+                }`}
+              >
+                {type.charAt(0).toUpperCase() + type.slice(1)}
+              </button>
             ))}
           </div>
-        )}
-      </div>
 
-      <div className="flex flex-col gap-4 w-full md:max-w-3xl">
-        <div
-          ref={playerRef}
-          className={`relative w-full aspect-video border border-[#111] p-4 rounded transition-all duration-300 ${
-            theatreMode
-              ? "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 max-w-[90vw] w-full"
-              : ""
-          }`}
-        >
-          {embedUrl ? (
-            <iframe
-              key={`embed-${embedUrl}`}
-              src={embedUrl}
-              allowFullScreen
-              className="w-full h-full aspect-video shadow-lg shadow-[#0008]"
+          <div className="relative w-full max-w-lg">
+            <input
+              type="url"
+              placeholder="Enter IMDb or TMDB URL"
+              value={url}
+              onChange={handleUrlChange}
+              className="w-full bg-[#111] text-white border border-[#181818] rounded px-4 py-2 pr-10 outline-none"
             />
-          ) : (
-            <div className="w-full h-full bg-black flex items-center justify-center text-[#888] text-md relative overflow-hidden shadow-lg shadow-[#0008]">
-              <div className="relative z-10 text-center">
-                No {mediaType} loaded. Tune in with a link.
-                <Link
-                  href="/how-to"
-                  className="align-super text-xs ml-1 text-[#666] hover:text-white transition-colors"
-                  title="Help or Info"
-                >
-                  [?]
-                </Link>
-              </div>
+            {url ? (
+              <button
+                onClick={() => setUrl("")}
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-white hover:text-gray-300"
+              >
+                <MdClose className="text-lg" />
+              </button>
+            ) : (
+              <button
+                onClick={async () => {
+                  try {
+                    const text = await navigator.clipboard.readText();
+                    if (text) setUrl(text);
+                  } catch (err) {
+                    console.error("Clipboard read failed:", err);
+                  }
+                }}
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-white hover:text-gray-300"
+              >
+                <IoMdClipboard className="text-lg" />
+              </button>
+            )}
+          </div>
+
+          {mediaType === "series" && (
+            <div className="space-y-2 w-full max-w-lg">
+              {[
+                {
+                  label: "Season",
+                  value: season,
+                  onChange: handleSeasonChange,
+                },
+                {
+                  label: "Episode",
+                  value: episode,
+                  onChange: handleEpisodeChange,
+                },
+              ].map(({ label, value, onChange }) => (
+                <div className="flex w-full" key={label}>
+                  <span className="w-24 text-center px-4 py-2 bg-[#111] border border-r-0 border-[#111] rounded-l text-sm">
+                    {label}
+                  </span>
+                  <input
+                    type="number"
+                    min={1}
+                    value={value}
+                    onChange={onChange}
+                    placeholder="1"
+                    className="flex-1 w-full bg-[#0d0d0d] text-white border border-l-0 border-[#111] rounded-r px-4 py-2 outline-none"
+                  />
+                </div>
+              ))}
             </div>
           )}
         </div>
 
-        {!theatreMode && embedUrl && (
-          <div className="flex gap-0 justify-center">
-            <button
-              onClick={handleCopy}
-              className="cursor-pointer px-4 py-2 bg-[#111] border border-[#181818] rounded-l-md hover:bg-[#222]"
-            >
-              <MdLink className="text-xl" />
-            </button>
-            <button
-              onClick={() => setTheatreMode(true)}
-              className="cursor-pointer px-4 py-2 bg-[#111] border border-[#181818] rounded-r-md hover:bg-[#222]"
-            >
-              <MdMonitor className="text-xl" />
-            </button>
+        <div className="flex flex-col gap-4 w-full md:max-w-3xl">
+          {!theatreMode && (
+            <div className="relative w-full aspect-video border border-[#111] p-4 rounded transition-all duration-300">
+              {embedUrl ? (
+                <iframe
+                  key={`embed-${embedUrl}`}
+                  src={embedUrl}
+                  allowFullScreen
+                  className="w-full h-full aspect-video shadow-lg shadow-[#0008]"
+                />
+              ) : (
+                <div className="w-full h-full bg-black flex items-center justify-center text-[#888] text-md relative overflow-hidden shadow-lg shadow-[#0008]">
+                  <div className="relative z-10 text-center">
+                    No {mediaType} loaded. Tune in with a link.
+                    <Link
+                      href="/how-to"
+                      className="align-super text-xs ml-1 text-[#666] hover:text-white transition-colors"
+                      title="Help or Info"
+                    >
+                      [?]
+                    </Link>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
+
+          {embedUrl && (
+            <div className="flex gap-0 justify-center">
+              <button
+                onClick={handleCopy}
+                className="cursor-pointer px-4 py-2 bg-[#111] border border-[#181818] rounded-md md:rounded-r-0 hover:bg-[#222]"
+              >
+                <MdLink className="text-xl" />
+              </button>
+              <button
+                onClick={() => setTheatreMode(true)}
+                className="cursor-pointer px-4 py-2 bg-[#111] border border-[#181818] rounded-r-md hover:bg-[#222]"
+              >
+                <MdMonitor className="text-xl" />
+              </button>
+            </div>
+          )}
+        </div>
+
+        {copySuccess && (
+          <div
+            className={`fixed bottom-6 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded shadow-lg flex items-center gap-2 z-[100] transition-all duration-500
+              ${
+                showToast
+                  ? "opacity-100 translate-y-0"
+                  : "opacity-0 translate-y-4 pointer-events-none"
+              }
+              bg-green-600 text-white`}
+          >
+            <MdCheckCircle className="text-xl" />
+            <span>{copySuccess}</span>
           </div>
         )}
       </div>
 
-      {embedUrl && (
-        <div
-          className={`fixed inset-0 z-40 bg-black transition-opacity duration-300 ${
-            theatreMode ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
-          }`}
-        />
+      {theatreMode && embedUrl && (
+        <>
+          <div
+            className="fixed inset-0 bg-black z-[1000] items-center justify-center flex"
+            onClick={() => setTheatreMode(false)}
+          >
+            <iframe
+              src={embedUrl}
+              allowFullScreen
+              className="w-full aspect-video object-cover"
+            />
+          </div>
+        </>
       )}
-
-      {copySuccess && (
-        <div
-          className={`fixed bottom-6 left-1/2 transform -translate-x-1/2 px-4 py-2 rounded shadow-lg flex items-center gap-2 z-[100] transition-all duration-500
-            ${
-              showToast
-                ? "opacity-100 translate-y-0"
-                : "opacity-0 translate-y-4 pointer-events-none"
-            }
-            bg-green-600 text-white`}
-        >
-          <MdCheckCircle className="text-xl" />
-          <span>{copySuccess}</span>
-        </div>
-      )}
-    </div>
+    </>
   );
 }
 
